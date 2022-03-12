9e3476df46038da69d5972dc79910c32794abf20
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2022-03-12 17:59:40 +0100

    read csv files,rearranging csv files

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import pandas as pd
 def read_dataset():
     df1 = pd.read_csv('data-set/BLI.csv')
     df2 = pd.read_csv('data-set/HSL.csv')
-    # print(df)
+    # df3 = pd.read_csv('data-set/data.csv')
+    df4 = pd.read_csv('data-set/BLI_new.csv')
+    df5 = pd.read_csv('data-set/HSL_new.csv')
+    # print(df3)
     # print(df1.head())
-    # print(df2.head())
-    print(df1.columns)
-    print(df2.columns)
+    print(df4.head())
+    print(df5.head())
+    # print(df1.columns)
+    # print(df2.columns)
     # print(df['LOCATION'])
 
 
