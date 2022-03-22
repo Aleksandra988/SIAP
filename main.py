42f3b2e9191a06ffa820ab871461236519a116d1
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2022-03-22 16:37:20 +0100

    fix up missing values

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pandas as pd
 
+
 def read_dataset():
-    df1 = pd.read_csv('data-set/BLI.csv')
-    df2 = pd.read_csv('data-set/HSL.csv')
-    # df3 = pd.read_csv('data-set/data.csv')
-    df4 = pd.read_csv('data-set/BLI_new.csv')
-    df5 = pd.read_csv('data-set/HSL_new.csv')
-    # print(df3)
-    # print(df1.head())
-    print(df4.head())
-    print(df5.head())
-    # print(df1.columns)
-    # print(df2.columns)
-    # print(df['LOCATION'])
+    df_bli = pd.read_csv('data-set/BLI.csv')
+    df_hsl = pd.read_csv('data-set/HSL.csv')
+    return df_bli, df_hsl
+
+
+def organize_missing_values(df11, df22):
+    df11 = df11.fillna(df11.median().round(1))
+    # print(df1)SS
+    i = 0
+    for value in df22.columns:
+        if df22[value].isnull().sum() > len(df22.index) / 2:
+            df22 = df22.drop(i)
+        i = i + 1
+    df22 = df22.reset_index()
+    df22 = df22.fillna(df22.median().round(1))
+    print(df22)
+    return df11, df22
 
 
 if __name__ == '__main__':
-    read_dataset()
-
+    df1, df2 = read_dataset()
+    df1, df2 = organize_missing_values(df1, df2)
