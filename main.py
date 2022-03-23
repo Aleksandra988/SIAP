b7eb1467f2db4fb11948adbab1a28d87b14d0d50
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2022-03-23 11:24:16 +0100

    solve missing values - corrections

--- main.py
+++ main.py
@@ -7,20 +7,28 @@ def read_dataset():
     return df_bli, df_hsl
 
 
-def organize_missing_values(df11, df22):
+def solve_missing_values(df11, df22):
     df11 = df11.fillna(df11.median().round(1))
-    # print(df1)SS
-    i = 0
-    for value in df22.columns:
-        if df22[value].isnull().sum() > len(df22.index) / 2:
-            df22 = df22.drop(i)
-        i = i + 1
+    # print(df1)
+
+    # deleting rows with 60% missing values
+    col_number = len(df22.columns) * .60
+    print((len(df22.index)))
+    df22 = df22[df22.isnull().sum(axis=1) < col_number]
+    print((len(df22.index)))
+
+    # deleting cols with 70% missing values
+    pct_null = df22.isnull().sum() / len(df22)
+    missing_features = pct_null[pct_null > 0.70].index
+    print(len(df22.columns))
+    df22 = df22.drop(missing_features, axis=1)
+    print(len(df22.columns))
+
     df22 = df22.reset_index()
     df22 = df22.fillna(df22.median().round(1))
-    print(df22)
     return df11, df22
 
 
 if __name__ == '__main__':
     df1, df2 = read_dataset()
-    df1, df2 = organize_missing_values(df1, df2)
+    df1, df2 = solve_missing_values(df1, df2)
