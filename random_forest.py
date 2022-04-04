7682c276a6cf3496f872ab3ade128e9a8b36648c
Aleksandra Milijevic <aleksandra.milijevic98@uns.ac.rs>
2022-04-04 15:24:32 +0200

    random forest algorithm - fill missing values corrections

--- random_forest.py
+++ random_forest.py
@@ -28,13 +28,13 @@ def rf(df1, df2, df3):
     X = df3[[i for i in df3.columns.tolist() if i != 'Overall rank' and i != 'Country or region' and i != 'rating']]
     y = df3['rating']
 
-    training, testing, training_labels, testing_labels = train_test_split(X, y, test_size=.25, random_state=42)
+    training, testing, training_labels, testing_labels = split_dataset(X, y)
 
     # Normalize the data
-    sc = StandardScaler()
+    # sc = StandardScaler()
     # normed_train_data = pd.DataFrame(sc.fit_transform(training), columns=X.columns)
     # normed_test_data = pd.DataFrame(sc.fit_transform(testing), columns=X.columns)
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(random_state=137)
     clf.fit(training, training_labels)
     preds = clf.predict(testing)
     print(clf.score(training, training_labels))
@@ -43,41 +43,39 @@ def rf(df1, df2, df3):
     # print(classification_report(testing_labels, clf.predict(testing)))
 
     # BLI.csv dataset - adding column rating from df3(2018.csv) to df1(BLI.csv) dataset
-    df = pd.merge(df1, drop_column(df3)[['Country or region', 'rating']], left_on='Country', right_on='Country or region', how='left')
-    df['rating'] = df['rating'].fillna(0)
+    df = pd.merge(df1, drop_column(df3)[['Country or region', 'rating']], left_on='Country',
+                  right_on='Country or region', how='left')
+    df['rating'] = df['rating'].fillna(df['rating'].median().round(1))
 
-    XX = df[[i for i in df.columns.tolist() if i != 'Country' and i != 'Country or region' and i != 'rating']]
+    X2 = df[[i for i in df.columns.tolist() if i != 'Country' and i != 'Country or region' and i != 'rating']]
     # print(X)
-    yy = df['rating']
+    y2 = df['rating']
 
-    trainingg, testingg, training_labelss, testing_labelss = train_test_split(XX, yy, test_size=.25, random_state=42)
-    rfc = RandomForestClassifier()
-    rfc.fit(trainingg, training_labelss)
-    predss = rfc.predict(testingg)
-    print(rfc.score(trainingg, training_labelss))
-    print(rfc.score(testingg, testing_labelss))
-    print("Accuracy:", metrics.accuracy_score(testing_labelss, predss))
+    training2, testing2, training_labels2, testing_labels2 = split_dataset(X2, y2)
+    clf.fit(training2, training_labels2)
+    preds2 = clf.predict(testing2)
+    print(clf.score(training2, training_labels2))
+    print(clf.score(testing2, testing_labels2))
+    print("Accuracy:", metrics.accuracy_score(testing_labels2, preds2))
 
     # HSL.csv dataset - adding column rating from df3(2018.csv) to df1(HSL.csv) dataset
-    dff = pd.merge(df2, drop_column(df3)[['Country or region', 'rating']], left_on='Country',
-                   right_on='Country or region', how='left')
-    # print(df)
-    dff['rating'] = dff['rating'].fillna(0)
-    XXX = dff[[i for i in dff.columns.tolist() if i != 'Country' and i != 'Country or region' and i != 'rating']]
-    # print(X)
-    yyy = dff['rating']
+    dff = pd.merge(df2, drop_column(df3)[['Country or region', 'rating']], left_on='Country', right_on='Country or region', how='left')
+    dff['rating'] = dff['rating'].fillna(dff['rating'].median().round(1))
+    X3 = dff[[i for i in dff.columns.tolist() if i != 'Country' and i != 'Country or region' and i != 'rating']]
+    y3 = dff['rating']
 
-    traininggg, testinggg, training_labelsss, testing_labelsss = train_test_split(XXX, yyy, test_size=.25,
-                                                                                  random_state=42)
-    # print(training_labels)
-    rff = RandomForestClassifier()
-    rff.fit(traininggg, training_labelsss)
-    predsss = rff.predict(testinggg)
-    print(rff.score(traininggg, training_labelsss))
-    print(rff.score(testinggg, testing_labelsss))
-    print("Accuracy:", metrics.accuracy_score(testing_labelsss, predsss))
+    training3, testing3, training_labels3, testing_labels3 = split_dataset(X3, y3)
+    clf.fit(training3, training_labels3)
+    preds3 = clf.predict(testing3)
+    print(clf.score(training3, training_labels3))
+    print(clf.score(testing3, testing_labels3))
+    print("Accuracy:", metrics.accuracy_score(testing_labels3, preds3))
 
 
 def drop_column(df3):
     return df3.drop(['Overall rank', 'Score', 'GDP per capita', 'Social support', 'Healthy life expectancy',
                      'Freedom to make life choices', 'Generosity', 'Perceptions of corruption'], axis=1)
+
+
+def split_dataset(X, y):
+    return train_test_split(X, y, test_size=.25, random_state=35)
